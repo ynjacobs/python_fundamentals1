8259398f3dbe7760f73dd4bb7ef78081142513e1
ynjacobs <ytj64611@gamil.com>
2019-05-06 12:46:16 -0400

    wrote code to output year born given age

--- exercise3.py
+++ exercise3.py
@@ -1,1 +1,17 @@
 print("What is your name?")
+user_name = input()
+print("Hello, {}".format(user_name))
+
+secret_password = "please"
+
+print("What's the password?")
+password_attempt = input()
+
+correct_or_not = (password_attempt == secret_password)
+
+print("That's {}!".format(correct_or_not))
+
+print("How old are you?")
+age = input()
+
+print("You were born in {}".format(2019 - int(age)))
