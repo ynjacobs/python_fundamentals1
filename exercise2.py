206b90d836279ae8384c4857c36a50aff5d3b4a3
ynjacobs <ytj64611@gamil.com>
2019-05-06 12:20:28 -0400

    answered the four questions using pyhton and anotated

---
+++ exercise2.py
@@ -0,0 +1,12 @@
+print("I would probably give an 18 percent tip because I think that's standard for a meal")
+# just using print to output my answer to the question
+
+#"hello" + 5 #trying to concatenate gave an error
+
+"hello" + str(5) #using str() converted the int to a string
+print("hello" + str(5))
+
+print("the result is {}".format(45628 * 7839)) #used interpolation to mutiply 2 numbers and put into a string
+
+print((10 < 20 and 30 < 20) or not(10==11)) #should be true based on the or and not statements
+
